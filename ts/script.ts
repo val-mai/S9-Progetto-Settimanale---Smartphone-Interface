ae74b428cdbfdc8fb3c223e95721c60317778e8a
val-mai <valerio.maiolini@gmail.com>
2022-06-24 15:39:14 +0200

    chiamata commit

--- ts/script.ts
+++ ts/script.ts
@@ -40,6 +40,7 @@ class User {
 
     azzeraChiamate(): void {
         this.numeroChiamate = 0;
+        this.durataTotale = 0;
     }
 
 };
@@ -48,10 +49,102 @@ let user1 = new User(15,0,0);
 let user2 = new User(10,0,0);
 let user3 = new User(20,0,0);
 
-let p = document.querySelector('.info1 p');
+function initialize() {
+    phoneBody!.innerHTML ='';
+    let info1 = document.createElement('div');
+    info1.classList.add('inf01');
+    let h2 = document.createElement('h2');
+    h2.innerHTML = 'Credito residuo'
+    let credito = document.createElement('p');
+    credito.innerHTML = user1.numero404().toFixed(2);
+    let info2 = document.createElement('div');
+    info2.classList.add('info2');
+    let h4_1 = document.createElement('h4');
+    h4_1.innerHTML = 'Chiamate effettuate';
+    let chiamate = document.createElement('p');
+    chiamate.innerHTML = `${user1.getNumeroChiamate()}`;
+    let h4_2 = document.createElement('h4');
+    h4_2.innerHTML = 'Durata totale delle chiamate';
+    let durataTot = document.createElement('p');
+    durataTot.innerHTML = user1.getDurataTotale();
+    let reset = document.createElement('div');
+    reset.classList.add('reset');
+    reset.innerHTML =  '<i class="bi bi-x-circle-fill"></i>';
+    let controls = document.createElement('div');
+    controls.classList.add('controls-bottom');
+    let call = document.createElement('img');
+    call.src = 'img/green.svg';
+    let utenze = document.createElement('img');
+    utenze.src = 'img/users.svg'
+    let ricarica = document.createElement('img');
+    ricarica.src = 'img/credit.svg'
 
-p!.innerHTML = `${user1.numero404()}€`;
+    phoneBody!.appendChild(info1);
+    info1.appendChild(h2);
+    info1.appendChild(credito);
+    phoneBody!.appendChild(info2);
+    info2.appendChild(h4_1);
+    info2.appendChild(chiamate);
+    info2.appendChild(h4_2);
+    info2.appendChild(durataTot);
+    info2.appendChild(reset);
+    phoneBody!.appendChild(controls);
+    controls.appendChild(call);
+    controls.appendChild(utenze);
+    controls.appendChild(ricarica);
 
-let p2 = document.querySelector('.info2 p');
+    reset.addEventListener('click', ()=>{
+        user1.azzeraChiamate();
+        initialize();
+    });
+    call.addEventListener('click', startCall);
 
-p2!.innerHTML = `${user1.getNumeroChiamate()}`;
+}
+
+initialize();
+
+function startCall() {
+    phoneBody!.innerHTML = '';
+    
+    let destinatario = document.createElement('div');
+    destinatario.classList.add('destinatario');
+    let img = document.createElement('img');
+    img.src = 'img/male.jpg';
+    let h3 = document.createElement('h3');
+    h3.innerHTML = 'John Doe';
+    let timer = document.createElement('p');
+    let controls = document.createElement('div');
+    controls.classList.add('controls-bottom');
+    let close = document.createElement('img');
+    close.src = 'img/red.svg';
+    phoneBody!.appendChild(destinatario);
+    destinatario.appendChild(img);
+    destinatario.appendChild(h3);
+    destinatario.appendChild(timer);
+    phoneBody!.appendChild(controls);
+    controls.appendChild(close);
+
+    let s: any = 0;
+    let m: any = 0;
+    let startTimer = setInterval(() => {
+        s++;
+        if (s === 60) {
+            s=0;
+            m++;
+            console.log(s);
+        }
+        if (s < 10) {
+            timer!.innerHTML = `${m}:0${s}`;
+        } else {
+            timer!.innerHTML = `${m}:${s}`;
+        }
+    }, 1000);
+
+    close.addEventListener('click', () => {
+        clearInterval(startTimer);
+        timer!.innerHTML = '';
+        let durata = s + (m*60);
+        user1.chiamata(durata);
+        initialize();
+    });
+}
